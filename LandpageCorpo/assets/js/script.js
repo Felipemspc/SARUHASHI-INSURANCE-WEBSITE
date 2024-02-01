2f05fadae4403b0aa17a2d5214012f85456a59d7
Felipemspc <mspcfelipe@gmail.com>
2024-02-01 16:53:30 -0300

    All set

--- LandpageCorpo/assets/js/script.js
+++ LandpageCorpo/assets/js/script.js
@@ -2,9 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "Images/menu_white_36dp.svg";
+        document.querySelector('.icon').src = "LandpageCorpo/Images/menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "Images/close_white_36dp.svg";
+        document.querySelector('.icon').src = "LandpageCorpo/Images/close_white_36dp.svg";
     }
 }
